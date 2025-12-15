0f7b5f3d9bd1bf7331f048292676dc99077cde1f
RobertPlaza12 <olivaresrobert1208@gmail.com>
2025-12-15 17:29:08 -0400

    mainMenu

---
+++ script/apiConfig.js
@@ -0,0 +1,14 @@
+// Archivo de configuración para todas las URLs
+const API_CONFIG = {
+    BASE_URL: "../api/",
+    ENDPOINTS: {
+        MAINMENU: "data/mainMenu.json",
+        USERS: "users",
+        PRODUCTS: "products"
+    },
+    /*
+    TIMEOUT: 5000,
+    VERSION: "v1"*/
+};
+
+export default API_CONFIG;
---
+++ script/mainMenu.js
@@ -0,0 +1,31 @@
+import API_CONFIG from 'apiConfig.js';
+
+async function cargarMenu() {
+    try {
+        const url = API_CONFIG.BASE_URL + API_CONFIG.ENDPOINTS.MAINMENU;
+        const response = await fetch(url);
+        
+        if (!response.ok) {
+            throw new Error(`Error HTTP: ${response.status}`);
+        }
+        
+        const menuData = await response.json();
+        const menus = document.getElementById("menu");
+        const ul = document.createElement("ul");
+
+        menuData.forEach(item => {
+            const li = document.createElement("li");
+            const a = document.createElement("a");
+            a.textContent = item.nombre;
+            a.href = item.url;
+            
+            li.appendChild(a);
+            ul.appendChild(li);
+        });
+
+        menus.appendChild(ul);
+    } catch (error) {
+        console.error("Error cargando el menú:", error);
+    }
+}
+cargarMenu();
